fd20bfe38744fdf9f8d7861301afd33cc55495f4
ketu <ketu.lai@gmail.com>
2019-09-30 10:40:26 +0800

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(current_path, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='laaiutils',
-    version='1.0.1',
+    version='1.0.2',
     description='Useful utils',
     long_description=long_description,
     url='https://github.com/ketu/laaiutils',
